d5f792245e71187f67704f40301c6d2fc321a5b9
soumia <soumia.sahtani@gmail.com>
2023-11-23 20:06:29 +0100

    add projet.html

---
+++ js/navbar.js
@@ -0,0 +1,15 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   var sidebarLinks = document.querySelectorAll(".sidebar a");
+
+//   sidebarLinks.forEach(function (sidebarLink) {
+//     sidebarLink.addEventListener("click", function (e) {
+//       e.preventDefault();
+
+//       sidebarLinks.forEach(function (otherSidebarLink) {
+//         otherSidebarLink.classList.remove("bg-gray-200");
+//       });
+
+//       this.classList.add("bg-gray-200");
+//     });
+//   });
+// });
